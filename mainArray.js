d57ebc8a789a6482f643b73155f779ca1bc114af
BRYAN PRINZ DA SILVA <bryan_prinz-silva@estudante.sesisenai.org.br>
2024-03-06 15:34:09 -0300

    Soma das array

---
+++ mainArray.js
@@ -0,0 +1,33 @@
+const readLineSync = require('readline-sync')
+
+let num1 = [1, 2, 3, 4, 5]
+let num2 = [ ]
+let soma = [ ]
+let contador = 0;
+let valor = 0
+
+function conta(){
+    while(contador <= 4){
+        valor = Number(readLineSync.question('Digite um valor para ser guardado: '))
+        num2.push(valor)
+        contador += 1
+    }
+    console.log('======================================')
+    return num2
+}
+function somar(){
+    for(let i = 0; i <= 4;i++){
+        soma[i] = num1[i] + num2[i]
+    }
+    return soma
+}
+function sub(){
+
+}
+console.log('======================================')
+console.log('     Soma dos valores de 2 Arrays     ')
+console.log('======================================')
+console.log('A Array 2 é: ',conta())
+console.log('======================================')
+console.log('O Array 3 é: ',sub())
+console.log('======================================')
